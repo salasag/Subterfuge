64eebf198552a985d652a12c48406191f581d876
Alex Salas <ags40@duke.edu>
2020-08-16 17:46:06 -0400

    new

--- server.js
+++ server.js
@@ -15,13 +15,14 @@ const io = socketIo(server);
 
 MisterMosby = new LobbyManager(io)
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
 
-// if (process.env.NODE_ENV === 'production') {
-// // Serve any static files
-// app.use(express.static(path.join(__dirname, 'client/build')));
-// // Handle React routing, return all requests to React app
-// app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
-// }
+if (process.env.NODE_ENV === 'production') {
+// Serve any static files
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+}
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
