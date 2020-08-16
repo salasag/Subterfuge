1268123e533f1860fc6b04c97d344d41a04a6d11
Alex Salas <ags40@duke.edu>
2020-08-16 16:17:24 -0400

    new

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
+  // res.send({ response: "I am alive" }).status(200);
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
-
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+// app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 module.exports = router;
